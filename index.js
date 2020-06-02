c8674901bc2f8b4f3be7919ac8286529f4cb500d
AleksaHrevtosva <hrevtosva@yahoo.com>
2020-06-02 11:44:36 +0300

    dom elements

--- index.js
+++ index.js
@@ -109,6 +109,31 @@ sum(banansPrice);
 // }
 // alerts();
 
-alert("1");
-alert("2");
-alert("3");
+// alert("1");
+// alert("2");
+// alert("3");
+
+// hhhhhhhhhhhhhhhhh
+let crlsRoot = document.querySelector(".ant-carousel");
+console.log(crlsRoot);
+
+let crlsList = document.querySelector(".ant-carousel-list");
+console.log(crlsList);
+
+let crlsElements = document.querySelectorAll(".ant-carousel-element");
+console.log(crlsElements);
+
+let crlsElemFirst = document.querySelector(".ant-carousel-element");
+console.log(crlsElemFirst);
+
+let leftArrow = document.querySelector("div.ant-carousel-arrow-left");
+console.log(leftArrow);
+
+let rightArrow = document.querySelector("div.ant-carousel-arrow-right");
+console.log(rightArrow);
+
+let indicatorDots = document.querySelector('.ant-carousel-dots');
+console.log(indicatorDots);
+
+
+
